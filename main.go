cc076ea88a73839cbc500f9a1ab4f7077938f28f
BaiYang <xliu779@foxmail.com>
2018-12-18 17:20:23 +0800

    使用for循环接收数据

--- main.go
+++ main.go
@@ -36,21 +36,23 @@ func main() {
 
 	fmt.Println("客户地址:", remoteAddr)
 
-	fmt.Println("等待接收数据...")
-
 	//定义一个数据接收Buffer
 	var buf [1024]byte
 
-	//读取数据
-	n, err := conn.Read(buf[0:])
+	for {
+		fmt.Println("等待接收数据...")
+		//读取数据
+		n, err := conn.Read(buf[0:])
 
-	if err != nil {
-		fmt.Println("数据接收错误, ", err)
+		if err != nil {
+			fmt.Println("数据接收错误, ", err)
+			return
+		}
+
+		//将bytes转为字符串
+		result := string(buf[0:n])
+
+		//输出结果
+		fmt.Println("接收到数据:", result)
 	}
-
-	//将bytes转为字符串
-	result := string(buf[0:n])
-
-	//输出结果
-	fmt.Println("接收到数据:", result)
 }
